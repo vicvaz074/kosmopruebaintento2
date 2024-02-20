baaf08f97f3ceb9a046843595b13b9d56784f096
Vicente Vázquez <128206250+vicvaz074@users.noreply.github.com>
2024-02-19 20:30:24 -0600

    Delete src directory

--- src/authRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { register, login } = require('./AuthController'); // Asegúrate de que la ruta sea correcta
-
-router.post('/registrarse', register);
-router.post('/login', login);
-
-module.exports = router;
