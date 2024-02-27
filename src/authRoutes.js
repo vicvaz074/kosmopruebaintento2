a579ca712f89f9256b6c01c11475ad2b4fb45b87
Vicente Vázquez <128206250+vicvaz074@users.noreply.github.com>
2024-02-26 21:57:49 -0600

    Update authRoutes.js

--- src/authRoutes.js
+++ src/authRoutes.js
@@ -1,8 +1,20 @@
 const express = require('express');
 const router = express.Router();
-const { register, login } = require('./AuthController'); // Asegúrate de que la ruta sea correcta
+const AuthController = require('./AuthController');
+const verifyToken = require('./authMiddleware');
 
-router.post('/registrarse', register);
-router.post('/login', login);
+// Ruta de registro
+router.post('/register', AuthController.register);
+
+// Ruta de login
+router.post('/login', AuthController.login);
+
+// Ruta para verificar el token de autenticación
+router.get('/verify-token', verifyToken, (req, res) => {
+  // Si el middleware `verifyToken` pasa, entonces el token es válido
+  // Puedes agregar cualquier lógica adicional aquí si es necesario
+  // Por ejemplo, devolver información del usuario, etc.
+  res.status(200).send({ message: "Token verificado con éxito", isAuthenticated: true });
+});
 
 module.exports = router;
